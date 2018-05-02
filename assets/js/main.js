b2d0d175ae9af93dcdf1a912c5e3cc6f1a3c2501
Stanescu Eduard-Dan <zflashare@gmail.com>
2018-05-02 16:14:47 +0300

    Upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	// Landing page - testimonial slider
+	$(".testimonial-wrapper").owlCarousel({
+		items: 1,
+		itemElement: "div.item",
+		loop: true,
+		autoplay: false
+	});
+
+});
