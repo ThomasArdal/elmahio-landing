0a454f272e30f5510d3751b3ebcfd82b91813f3f
Stanescu Eduard-Dan <zflashare@gmail.com>
2018-05-28 15:56:40 +0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -24,15 +24,44 @@ $(document).ready(function(){
 		}
 	});
 
-	// Scrollspy
-	$('body').scrollspy({
-		target: '.navbar-special',
-		offset: 65
+	$('a.js-scroll-trigger-docs[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - 0)
+				}, 500);
+				return false;
+			}
+		}
 	});
+
+	// Scrollspy
+	if ($('.navbar-special').length){
+		$('body').scrollspy({
+			target: '.navbar-special',
+			offset: 65
+		});
+	}
+	if ($('.sidebar-docs').length){
+		$('body').scrollspy({
+			target: '.sidebar-docs',
+			offset: 10
+		});
+	}
 
 	// Enable popovers
 	$('[data-toggle="popover"]').popover({
 		container: 'body'
 	});
+
+	// Hljs
+	$('pre code').each(function(i, block) {
+		hljs.highlightBlock(block);
+	});
+	$('code.hljs.linenumber').each(function(i, block) {
+        hljs.lineNumbersBlock(block);
+    });
 
 });
